7426eed0fb33e4f0a81c953f698861a8462f1577
sole <macore@163.com>
2024-05-12 13:25:28 +0800

    feat: bugfix

--- src/modules/manager/dto/update-manager.dto.ts
+++ src/modules/manager/dto/update-manager.dto.ts
@@ -1,10 +1,14 @@
 import { CreateManagerDto } from './create-manager.dto';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
 
 export class UpdateManagerDto extends CreateManagerDto {
   @ApiProperty({ description: 'id', example: 1 })
   @IsNotEmpty({ message: 'id不能为空' })
   @IsNumber(undefined, { message: 'id为数字' })
   id: number;
+
+  @ApiPropertyOptional({ description: '密码', example: 'admin' })
+  @IsOptional()
+  password: string;
 }
